51cbfd7db4af31f40967e289468c6638a71871c1
Keloo <vlad-negura@mail.ru>
2017-03-07 16:57:30 +0200

    Adding fosuserbundle, vagrant configuration, other

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseModel;
+
+/**
+ * User
+ */
+class User extends BaseModel
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
